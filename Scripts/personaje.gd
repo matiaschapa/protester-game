815f467aaa7e2df7f4271c1e661f3cf01c3f3c82
Matias Chaparro <chaparromatiasjavier@gmail.com>
2025-04-18 15:27:11 -0300

    movimiento

--- Scripts/personaje.gd
+++ Scripts/personaje.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+var velocidad = 1000
+
+func _process(delta):
+	
+	if Input.is_action_pressed("arriba"):
+		position.y -= velocidad * delta
+	
+	if Input.is_action_pressed("abajo"):
+		position.y += velocidad * delta
+		
+	if Input.is_action_pressed("izquierda"):
+		position.x -= velocidad * delta
+		
+	if Input.is_action_pressed("derecha"):
+		position.x += velocidad * delta
